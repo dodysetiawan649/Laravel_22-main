fd62865cb2ebb0f70b7f7049d72fa97bba6cc648
dodysetiawan649 <222220108@unmuhpnk.ac.id>
2024-06-09 12:43:56 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/pengguna/{user_id}',[UserController::class,'tampilData']);
+// Route::get('/pengguna',[UserController::class,'index']);
+// Route::get('/pengguna/create', [UserController::class,'create']);
+
+Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
+
